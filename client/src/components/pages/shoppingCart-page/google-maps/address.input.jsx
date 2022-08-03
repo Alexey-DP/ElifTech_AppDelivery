f1386adc880337775831101676cececf41403e1f
Alexey Pochtovik <alexeydp92dp@gmail.com>
2022-08-03 16:26:18 +0300

    new style

--- client/src/components/pages/shoppingCart-page/google-maps/address.input.jsx
+++ client/src/components/pages/shoppingCart-page/google-maps/address.input.jsx
@@ -39,17 +39,21 @@ const AddressInput = ({isLoaded, onSelect}) => {
             };
 
     const renderSuggestions = () =>
-        data.map((suggestion) => {
-            const {
-                place_id,
-                structured_formatting: { main_text, secondary_text },
-            } = suggestion;
+        data.map((suggestion, index) => {
+            if(index < 2) {
+                const {
+                    place_id,
+                    structured_formatting: { main_text, secondary_text },
+                } = suggestion;
 
-            return (
-                <li key={place_id} onClick={handleSelect(suggestion)}>
-                    <strong>{main_text}</strong> <small>{secondary_text}</small>
-                </li>
-            );
+                return (
+                    <li key={place_id} onClick={handleSelect(suggestion)}>
+                        <strong>{main_text}</strong> <small>{secondary_text}</small>
+                    </li>
+                );
+            } else {
+                return null;
+            }
         });
 
         useEffect(() => {
